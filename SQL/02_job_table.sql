aac5f289cc03a2f524de16986b85f21453078f15
Yamil Llanos <yamil.llanos@gmail.com>
2017-09-25 22:10:08 -0500

    Adding code documentation

--- SQL/02_job_table.sql
+++ SQL/02_job_table.sql
@@ -2,16 +2,25 @@
 --  DDL for Table JOB
 --------------------------------------------------------
 
-  CREATE TABLE "JOB" 
-   (	
-    "JOB_NAME"              VARCHAR2(255 BYTE), 
-	"SUBJECT_AREA"          VARCHAR2(50 BYTE), 
-	"ETL"                   NUMBER, 
-	"JOB_DSC"               VARCHAR2(255 BYTE), 
-	"JOB_NAME_DS"           VARCHAR2(255 BYTE), 
-	"JOB_NAME_PL"           VARCHAR2(255 BYTE), 
-	"JOB_PATH"              VARCHAR2(255 BYTE), 
-	"JOB_ID"                NUMBER, 
-	"JOB_TYPE"              NUMBER, 
+/*******************************************************
+
+  DESCRIPTION:      Utility table where main procedure keeps track of job names
+  INPUT PARAMS:     N/A
+  OUTPUT PARAMS:    N/A
+  COMMENTS:
+
+********************************************************/
+
+  CREATE TABLE "JOB"
+   (
+    "JOB_NAME"              VARCHAR2(255 BYTE),
+	"SUBJECT_AREA"          VARCHAR2(50 BYTE),
+	"ETL"                   NUMBER,
+	"JOB_DSC"               VARCHAR2(255 BYTE),
+	"JOB_NAME_DS"           VARCHAR2(255 BYTE),
+	"JOB_NAME_PL"           VARCHAR2(255 BYTE),
+	"JOB_PATH"              VARCHAR2(255 BYTE),
+	"JOB_ID"                NUMBER,
+	"JOB_TYPE"              NUMBER,
 	"JOB_TYPE_DESC"         VARCHAR2(12 BYTE)
    )  ;
